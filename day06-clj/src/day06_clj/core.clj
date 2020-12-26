3198aa2d23e1fd325851b463f849bd60d53400bb
Gian Lorenzo Meocci <glmeocci@gmail.com>
2020-12-26 21:30:21 +0100

    refactoring

--- day06-clj/src/day06_clj/core.clj
+++ day06-clj/src/day06_clj/core.clj
@@ -6,21 +6,23 @@
 (defn count-yes [replies]
   (count (reduce (fn [acc x] (conj acc x)) #{} (string/split replies #""))))
 
-(def load-input-anyone
+(def load-input
   (with-open [rdr (clojure.java.io/reader "./data/input.txt")]
-    (filter
-      (fn [x] (not-empty x))
-      (map
-        (fn [xs] (string/join "" xs))
-        (partition-by #(= "" %) (doall (line-seq rdr)))))))
+    (doall (line-seq rdr))))
+
+(def load-input-anyone
+  (filter
+    (fn [x] (not-empty x))
+    (map
+      (fn [xs] (string/join "" xs))
+      (partition-by #(= "" %) load-input))))
 
 (def sum-of-counts (apply + (map count-yes load-input-anyone)))
 
 (def load-input-everyone
-  (with-open [rdr (clojure.java.io/reader "./data/input.txt")]
-    (filter
-      (fn [xs] (not= '("") xs))
-      (partition-by #(= "" %) (doall (line-seq rdr))))))
+  (filter
+    (fn [xs] (not= '("") xs))
+    (partition-by #(= "" %) load-input)))
 
 (defn group-set [xs]
   (map (fn [x] (set x)) xs))
