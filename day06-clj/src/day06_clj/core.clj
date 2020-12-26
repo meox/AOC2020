fe24943e53d5bacdf9f190f70d434e1e79bc3b20
Gian Lorenzo Meocci <glmeocci@gmail.com>
2020-12-26 18:05:57 +0100

    part 2

--- day06-clj/src/day06_clj/core.clj
+++ day06-clj/src/day06_clj/core.clj
@@ -16,7 +16,22 @@
 
 (def sum-of-counts (apply + (map count-yes load-input-anyone)))
 
+(def load-input-everyone
+  (with-open [rdr (clojure.java.io/reader "./data/input.txt")]
+    (filter
+      (fn [xs] (not= '("") xs))
+      (partition-by #(= "" %) (doall (line-seq rdr))))))
 
+(defn group-set [xs]
+  (map (fn [x] (set x)) xs))
+
+(defn common-group-reply [xs]
+  (apply clojure.set/intersection (group-set xs)))
+
+(defn count-common-reply [xs]
+  (count (common-group-reply xs)))
+
+(def sum-of-counts-everyone (apply + (map count-common-reply load-input-everyone)))
 
 (defn -main
   "I don't do a whole lot ... yet."
