0c9bce87316a3fca1bfe06d7f665892a3e36b976
Gian Lorenzo Meocci <glmeocci@gmail.com>
2020-12-26 17:47:52 +0100

    part 1

--- day06-clj/src/day06_clj/core.clj
+++ day06-clj/src/day06_clj/core.clj
@@ -1,6 +1,23 @@
 (ns day06-clj.core
   (:gen-class))
 
+(require '[clojure.string :as string])
+
+(defn count-yes [replies]
+  (count (reduce (fn [acc x] (conj acc x)) #{} (string/split replies #""))))
+
+(def load-input-anyone
+  (with-open [rdr (clojure.java.io/reader "./data/input.txt")]
+    (filter
+      (fn [x] (not-empty x))
+      (map
+        (fn [xs] (string/join "" xs))
+        (partition-by #(= "" %) (doall (line-seq rdr)))))))
+
+(def sum-of-counts (apply + (map count-yes load-input-anyone)))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
